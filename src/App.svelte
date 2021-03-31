0d013c85ba06a957f34ada42adb0c50af567407c
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-03-31 13:06:27 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+
+<script>
+	import { onMount } from 'svelte';
+	import { Paella } from 'paella-core';
+
+	onMount(async () => {
+		const paella = new Paella('player-container');
+
+		paella.loadManifest()
+			.then(() => console.log("Done"))
+			.catch(err => console.log(err));
+	});
+</script>
+
+<main>
+	<h1>Hello from Svelte</h1>
+	<div id="player-container"></div>
+</main>
+
+<style>
+	div {
+		width: 800px;
+		height: 600px;
+	}
+</style>
